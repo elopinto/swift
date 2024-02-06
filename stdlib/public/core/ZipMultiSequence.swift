e71d67333969d250727280ad9ed144a03995fd62
Edward LoPinto <e.e.lopinto@gmail.com>
2024-02-06 12:05:48 -0500

    [stdlib] Improve ZipMultiSequence's conformance to stdlib conventions.
    Add @frozen and @inlinable attributes to ZipMultiSequence's
    members to match the convention used throughout the standard
    library. Also, add availability attributes.

--- stdlib/public/core/ZipMultiSequence.swift
+++ stdlib/public/core/ZipMultiSequence.swift
@@ -42,7 +42,8 @@
 /// - Parameter sequence: A comma-separated list of sequences to zip.
 /// - Returns: A sequence of tuples, where the elements of each tuple are
 ///   corresponding elements of the input sequences.
-@available(macOS 14.0, *)
+@available(SwiftStdlib 9999, *)
+@inlinable
 public func zip<each Chain: Sequence>(
   sequences chain: repeat each Chain
 ) -> ZipMultiSequence<repeat each Chain> {
@@ -57,20 +58,24 @@ public func zip<each Chain: Sequence>(
 ///
 /// Use the `zip(sequences:)` function to create a `ZipMultiSequence`
 /// instance.
-@available(macOS 14.0, *)
+@available(SwiftStdlib 9999, *)
+@frozen
 public struct ZipMultiSequence<each Chain: Sequence>: Sequence {
-  
+
   /// A value pack containing the input sequences, or "chains of the zipper".
-  private let _chain: (repeat each Chain)
+  @usableFromInline
+  internal let _chain: (repeat each Chain)
 
   /// Creates an instance of `ZipMultiSequence` from a tuple of zero or more
   /// sequences.
   /// - Parameter chain: A tuple of zero or more sequences.
-  fileprivate init(_ chain: (repeat each Chain)) {
+  @inlinable
+  internal init(_ chain: (repeat each Chain)) {
     self._chain = chain
   }
 
   /// Return an iterator for this sequence.
+  @inlinable
   public __consuming func makeIterator() -> Iterator {
     return Iterator(
       iterator: (repeat AnyIterator((each _chain).makeIterator()))
@@ -78,6 +83,7 @@ public struct ZipMultiSequence<each Chain: Sequence>: Sequence {
   }
 
   /// The value of the shortest input sequence.
+  @inlinable
   public var underestimatedCount: Int {
     var underestimatedCounts: [Int] = []
     (repeat underestimatedCounts.append((each _chain).underestimatedCount))
@@ -85,11 +91,12 @@ public struct ZipMultiSequence<each Chain: Sequence>: Sequence {
   }
 }
 
-@available(macOS 14.0, *)
+@available(SwiftStdlib 9999, *)
 extension ZipMultiSequence {
-  
+
   /// An iterator for `ZipMultiSequence`.
-  @available(macOS 14.0, *)
+  @available(SwiftStdlib 9999, *)
+  @frozen
   public struct Iterator: IteratorProtocol {
 
     // When attempting to call to call `next()` on the input iterators during
@@ -98,15 +105,18 @@ extension ZipMultiSequence {
     // after type-erasing them to `AnyIterator`.
     /// A value pack holding the iterators of the underlying sequences of a
     /// `ZipMultiSequence`.
-    private let _chainIterator: (repeat AnyIterator<(each Chain).Element>)
+    @usableFromInline
+    internal let _chainIterator: (repeat AnyIterator<(each Chain).Element>)
 
     /// Set to true when the first underlying sequence finishes iterating.
-    private var _reachedEnd = false
+    @usableFromInline
+    internal var _reachedEnd = false
 
     /// Creates an instance of an interator for a `ZipMultiSequence`.
     /// - Parameter iterator: A tuple of zero or more type-erased iterators from
     /// a `ZipMultiSequence`'s underlying sequences.
-    fileprivate init(iterator: (repeat AnyIterator<(each Chain).Element>)) {
+    @inlinable
+    internal init(iterator: (repeat AnyIterator<(each Chain).Element>)) {
       self._chainIterator = iterator
     }
 
@@ -117,6 +127,7 @@ extension ZipMultiSequence {
     /// iterators returns `nil`.
     /// - Returns: A tuple containing an item from each underlying iterator or
     /// `nil` when iteration ends.
+    @inlinable
     public mutating func next() -> (repeat (each Chain).Element)? {
       // `ZipMultiSequence` stops iterating when the shortest underlying
       // sequence has reached its end. We check here if the sequence has
@@ -136,11 +147,13 @@ extension ZipMultiSequence {
   }
 }
 
-@available(macOS 14.0, *)
-private extension ZipMultiSequence {
-  /// Thrown when an underlying iterator's `next()` returns nil during value
-  /// pack expansion in `ZipMultiSequence.Iterator.next()`.
-  fileprivate struct _NextIsNil: Error {}
+@available(SwiftStdlib 9999, *)
+extension ZipMultiSequence.Iterator {
+  @frozen
+  @usableFromInline
+  internal struct _NextIsNil: Error {
+    @inlinable internal init() {}
+  }
 
   /// Return the wrapped value or throw an error if it is `nil`.
   ///
@@ -149,7 +162,8 @@ private extension ZipMultiSequence {
   ///
   /// - Returns: The wrapped value.
   /// - Throws: A `_NextIsNil`error if `self` is `nil`.
-  fileprivate static func _someOrThrow<Wrapped>(
+  @inlinable
+  internal func _someOrThrow<Wrapped>(
     _ wrapped: Wrapped?
   ) throws -> Wrapped {
     guard let wrapped else { throw _NextIsNil() }
@@ -157,13 +171,14 @@ private extension ZipMultiSequence {
   }
 }
 
-@available(macOS 14.0, *)
+
+@available(SwiftStdlib 9999, *)
 extension ZipMultiSequence: Sendable where repeat each Chain: Sendable {}
 
 // warning: stored property '_chainIterator' of 'Sendable'-conforming struct
 // 'Iterator' has non-sendable type '(repeat AnyIterator<(each Chain).Element>)'
 // Possible solutions: Make AnyIterator conform to Sendable? Find way to mutate
 // iterators in value packs?
-// @available(macOS 14.0, *)
+// @available(SwiftStdlib 9999, *)
 // extension ZipMultiSequence.Iterator: Sendable
 //  where repeat each Chain: Sendable, repeat (each Chain).Iterator: Sendable {}
